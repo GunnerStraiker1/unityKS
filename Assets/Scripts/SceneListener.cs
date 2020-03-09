05213739d6f7f24c2e0611a7395868ebb1005b3e
GunnerStraiker1 <victorcito001@hotmail.com>
2020-03-09 11:58:36 -0600

    V-FirstMenu

---
+++ Assets/Scripts/SceneListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneListener : MonoBehaviour
+{
+
+    public void navigate(string scene){
+        Debug.Log(scene);
+        SceneManager.LoadScene(scene);
+    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
